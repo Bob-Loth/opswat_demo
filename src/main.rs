e54eab80ae17b4cdfb1584f4cf0b32f1940f802a
Bob-Loth <36461371+Bob-Loth@users.noreply.github.com>
2022-08-23 13:41:08 -0700

    added main logic
    Currently just prints out response data.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::crypto::sha256_from_bytes;
-use sha2::Sha256;
+use crate::network::KeyedClient;
 use std::fs;
 use std::path::Path;
 
@@ -8,11 +8,22 @@ mod network;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     //1. Calculate the hash of a given file (i.e. samplefile.txt)
-    let contents = fs::read(Path::new("tests/resources/test.txt"))?;
+    let path = Path::new("tests/resources/test.txt");
+    let contents = fs::read(path)?;
     let hash = sha256_from_bytes(&contents)?;
     println!("{}", hash);
     //2. Perform a hash lookup against metadefender.opswat.com and see if there are
     //previously cached results for the file
+
+    let client = KeyedClient::new()?;
+    if client.hash_exists(&hash)? {
+        //print out response
+    } else {
+        //a valid not found response
+        let json_data = client.upload_file(path)?;
+        println!("{:#?}", json_data);
+    }
+
     //3. If results are found, skip to step 6
     //4. If results are not found, upload the file and receive a "data_id"
     //5. Repeatedly pull on the "data_id" to retrieve results
