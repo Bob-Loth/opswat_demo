8320a896c8f1b339a831e40c451834ad919234ea
Bob-Loth <36461371+Bob-Loth@users.noreply.github.com>
2022-08-22 22:50:31 -0700

    changed hex display implementation, removed hex crate.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,10 +1,17 @@
 use sha2::{Sha256, Digest};
 use sha2::digest::Output;
-use hex::ToHex;
-pub(crate) fn sha256_from_bytes(bytes: &[u8]) -> String  {
+use std::fmt::Write;
+pub(crate) fn sha256_from_bytes(bytes: &[u8]) -> Result<String,std::fmt::Error>  {
     let mut hasher = Sha256::new();
     hasher.update(bytes);
-    let val = hasher.finalize();
+    let val = hasher.finalize(); //a byte vector.
 
-    hex::encode_upper(val)
+    //hex formatting
+    let mut s = String::with_capacity(2 * val.len());
+    for byte in val {
+        //this really shouldn't error unless sha2's implementation is bad
+        write!(s, "{:02X}", byte)?;
+    }
+
+    Ok(s)
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ mod network;
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     //1. Calculate the hash of a given file (i.e. samplefile.txt)
     let contents = fs::read(Path::new("tests/resources/test.txt"))?;
-    let hash = sha256_from_bytes(&contents);
+    let hash = sha256_from_bytes(&contents)?;
+    println!("{}",hash);
     //2. Perform a hash lookup against metadefender.opswat.com and see if there are
     //previously cached results for the file
     //3. If results are found, skip to step 6
